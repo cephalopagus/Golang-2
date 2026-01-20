182cf3bd42252c86a5e9ffaf1e581801699cc9e4
cephalopagus <dastan.bak@gmail.com>
2026-01-21 03:28:31 +0600

    reflection

---
+++ Reflection/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	Inspect(12, 22, 21)
+	Inspect("Strochka")
+}
+
+func Inspect(i ...any) {
+	typeR := reflect.TypeOf(i)
+	valueR := reflect.ValueOf(i)
+
+	fmt.Println(typeR, valueR, typeR.Kind())
+}
