6dc881742bb9384f616d36670cdb3b7451a1e6a3
cephalopagus <dastan.bak@gmail.com>
2026-02-06 17:37:13 +0600

    channals practice

--- Channal/main.go
+++ Channal/main.go
@@ -2,19 +2,30 @@ package main
 
 import "fmt"
 
-func main() {
+func writer() chan int {
 	ch := make(chan int)
+	go func() {
 
-	for i := range 5 {
-		go func() {
+		defer close(ch)
+
+		for i := range 10 {
 			ch <- i + 1
-		}()
+		}
+		// ch <- 1
+		// ch <- 2
+	}()
+	return ch
+}
+
+func main() {
+
+	ch := writer()
+
+	for {
+		v, ok := <-ch
+		if !ok {
+			break
+		}
+		fmt.Println(v)
 	}
-	for range 5 {
-
-		reading := <-ch
-		fmt.Println(reading)
-
-	}
-
 }
